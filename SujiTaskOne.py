d8ee89ac95c8bbc817503334726ad3d1366c870a
Sujinthan <s.sujinthan@gmail.com>
2017-03-25 19:02:23 -0400

    extracted json from requests

--- SujiTaskOne.py
+++ SujiTaskOne.py
@@ -1,4 +1,7 @@
 import requests
 
 r = requests.get('https://api.typeform.com/v1/form/PfSNFM?key=ac83034cfa742c0f79c26e9a612b4ba7e2aa0d3d')
-print(r.json())
+data = r.json()
+
+for item in data['questions']:
+    print (item['question'])
