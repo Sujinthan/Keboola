3e96c03b105ab137e1b2d3c4a043af62872ab5b9
Sujinthan <s.sujinthan@gmail.com>
2017-03-26 01:59:50 -0400

    matched question with answers

--- SujiTaskOne.py
+++ SujiTaskOne.py
@@ -2,13 +2,13 @@ import requests
 
 r = requests.get('https://api.typeform.com/v1/form/PfSNFM?key=ac83034cfa742c0f79c26e9a612b4ba7e2aa0d3d')
 data = r.json()
-
+question_id =[]
+answer_dic = None
 for item in data ['questions']:
-    print(item['field_id'])
-    for thins in data['responses']:
-        print("\n")
-        print("question")
-        print(item['question'])
-        for id in thins['answers'].keys():
+    print("\n")
+    print(item['question'])
+    '''for id in question_id:'''
+    for response in data['responses']:
+        for id in response['answers']:
             if str(item['field_id']) in str(id):
-                print(thins['answers'][id])
+                print(response['answers'][id])
